0722dd0b6285de2cdace2533fa3321c0c70c6ca7
Pirraio <g.teixeira7x@gmail.com>
2022-12-20 14:51:49 -0300

    Adição dos planetas

--- pages/threejs_pages/js3d/o2.js
+++ pages/threejs_pages/js3d/o2.js
@@ -8,11 +8,13 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight , 0.1, 1000);
 camera.position.z = 10;
 
-const canvas = document.querySelector('#o2');
-const renderer = new THREE.WebGLRenderer({canvas});
+const canvas = document.querySelector('canvas.webgl');
+const renderer = new THREE.WebGLRenderer({
+	canvas: canvas
+});
 const sizes = {
-    width: window.innerWidth/2,
-    height: window.innerHeight/2
+    width: window.innerWidth*0.795,
+    height: window.innerHeight*0.8
 }
 
 renderer.setSize(sizes.width, sizes.height)
